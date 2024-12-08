abce28e7a20944eea80335ca209c2938b7e6db0e
a1exx2a <a1exx2a@yandex.ru>
2024-12-08 17:38:47 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] x = {1,2,3,4,5};
+        
+        Array.Reverse(x);
+        foreach (int i in x) {
+            Console.WriteLine(i);           
+        }
+    }
+}
